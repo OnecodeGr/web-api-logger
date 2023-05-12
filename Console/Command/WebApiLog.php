d15a382bb6dd57f782bb601dbb54c8aeeef31078
spyrmpontinis <spyrmpontinis@gmail.com>
2023-05-12 14:16:50 +0300

    Fixes #2

--- Console/Command/WebApiLog.php
+++ Console/Command/WebApiLog.php
@@ -17,36 +17,14 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\App\State;
 use Magento\Framework\App\Area;
-use Magento\Framework\Registry;
 
 /**
  * Class WebApiLog
  * @package Onecode\WebApiLogger\Console\Command
  */
 class WebApiLog extends Command {
-	/**
-	 * @var State
-	 */
-	private $_state;
 
 
-
-	/**
-	 * WebApiLog constructor.
-	 *
-	 * @param State    $state
-	 * @param Registry $registry
-	 * @param null     $name
-	 */
-	public function __construct ( State $state, $name = null ) {
-		parent::__construct( $name );
-		$this->_state    = $state;
-	}
-
-
-	/**
-	 *
-	 */
 	protected function configure () {
 		parent::configure();
 		$this->setName( "onecode:web:logger:cleaner" )
@@ -62,12 +40,12 @@ class WebApiLog extends Command {
 	 * @throws LocalizedException
 	 */
 	protected function execute ( InputInterface $input, OutputInterface $output ) {
-
-		try {
-			$this->_state->getAreaCode();
-		} catch ( LocalizedException $e ) {
-			$this->_state->setAreaCode( Area::AREA_CRONTAB );
-		}
+        $state = ObjectManager::getInstance()->get(State::class);
+        try {
+            $state->getAreaCode();
+        } catch (\Exception $e) {
+            $state->setAreaCode(Area::AREA_CRONTAB);
+        }
         ObjectManager::getInstance()->create( CleanUp::class )->execute();
 	}
 
